0d49efc867af99b7d430c68e9a0da2e07ccbbfeb
cjcobra <cjcobra2@gmail.com>
2019-12-10 11:54:28 -0500

    Flashing Red Works

---
+++ Project_F/Assets/Scripts/Flash2.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flash2 : MonoBehaviour
+{
+
+    public Image image;
+    public static string colorFlashing;
+    public float timer;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start()");
+        timer = 0.5f;
+
+        // Default                  
+        Invoke("ChangeColorToRed", timer);
+    }
+
+    public void stopTimer()
+    {
+        timer = 50000f;
+    }
+
+    public void fastTimer()
+    {
+       
+        timer = 0.5f;
+    }
+
+    public void fasterTimer()
+    {
+        timer = 0.25f;
+    }
+
+    public void fastestTimer()
+    {
+        timer = 0.10f;
+    }
+
+    ///////  RED /////////////
+    public void ChangeColorToRed()
+    {
+        if (timer > 1)
+        {
+            image.color = Color.red;
+
+        }
+        else
+        {
+            image.color = Color.red;
+            Invoke("ChangeColorToRedBlack", timer);
+        }
+    }
+
+    public void ChangeColorToRedBlack()
+    {
+        image.color = Color.black;
+        Invoke("ChangeColorToRed", timer);
+    }
+    ///////  END RED /////////////
+
+    ///////  BLUE /////////////
+    public void ChangeColorToBlue()
+    {
+        image.color = Color.blue;
+        Invoke("ChangeColorToBlueBlack", timer);
+    }
+
+    public void ChangeColorToBlueBlack()
+    {
+        image.color = Color.black;
+        Invoke("ChangeColorToBlue", timer);
+    }
+    ///////  END BLUE /////////////
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+
+    
+}
---
+++ Project_F/Assets/Scripts/Flash3.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flash3 : MonoBehaviour
+{
+
+    public Image image;
+    public string colorFlashing;
+    public float timer;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("start()");
+        timer = 0.5f;
+
+        // Default                  
+        StartCoroutine(ChangeColorToRed());
+    }
+
+    public void stopTimer()
+    {
+        Debug.Log("stopTimer()");
+        StopAllCoroutines();
+
+        if (colorFlashing == "Red")
+        {
+            image.color = Color.red;
+        }
+        if (colorFlashing == "Blue")
+        {
+            image.color = Color.blue;
+        }
+
+    }
+
+    public void fastTimer()
+    {
+        StopAllCoroutines();
+        if (colorFlashing == "Red")
+        {
+            StartCoroutine(ChangeColorToRed());
+        }
+
+        timer = 0.5f;
+    }
+
+    public void fasterTimer()
+    {
+        StopAllCoroutines();
+        if (colorFlashing == "Red")
+        {
+            StartCoroutine(ChangeColorToRed());
+        }
+
+        timer = 0.25f;
+    }
+
+    public void fastestTimer()
+    {
+        StopAllCoroutines();
+        if (colorFlashing == "Red")
+        {
+            StartCoroutine(ChangeColorToRed());
+        }
+
+        timer = 0.10f;
+    }
+
+    ///////  RED /////////////
+    public IEnumerator ChangeColorToRed()
+    {
+        Debug.Log("ChangeColorToRed");
+        colorFlashing = "Red";
+        image.color = Color.red;
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(ChangeColorToRedBlack());
+    }
+
+    public IEnumerator ChangeColorToRedBlack()
+    {
+        Debug.Log("ChangeColorToRedBlack");
+        image.color = Color.black;
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(ChangeColorToRed());
+    }
+    ///////  END RED /////////////
+
+    ///////  BLUE /////////////
+    public IEnumerator ChangeColorToBlue()
+    {
+        colorFlashing = "Blue";
+        image.color = Color.blue;
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(ChangeColorToBlueBlack());
+    }
+
+    public IEnumerator ChangeColorToBlueBlack()
+    {
+        image.color = Color.black;
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(ChangeColorToBlue());
+    }
+    ///////  END BLUE /////////////
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+
+    
+}
