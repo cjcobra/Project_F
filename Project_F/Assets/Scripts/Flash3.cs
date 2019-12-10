1281b50b6d6db5236cfc93da1e4106fcdab2625b
cjcobra <cjcobra2@gmail.com>
2019-12-10 14:10:28 -0500

    Full working app, no ads

--- Project_F/Assets/Scripts/Flash3.cs
+++ Project_F/Assets/Scripts/Flash3.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;  // CJ ADDED /////////////////////////////////////////////////////////////////////////////////////////
 
 public class Flash3 : MonoBehaviour
 {
@@ -9,18 +10,87 @@ public class Flash3 : MonoBehaviour
     public Image image;
     public string colorFlashing;
     public float timer;
-
-    
-
-    // Start is called before the first frame update
+    public Text textToCopy;
+    public Text FlashRText;
+      
     void Start()
     {
-        //Debug.Log("start()");
         timer = 0.5f;
 
         // Default                  
         StartCoroutine(ChangeColorToRed());
     }
+
+    public void startRed()
+    {
+        Debug.Log("startRed");
+        StopAllCoroutines();
+        StartCoroutine(ChangeColorToRed());
+    }
+    public void startBlue()
+    {
+        Debug.Log("startBlue");
+        StopAllCoroutines();
+        StartCoroutine(ChangeColorToBlue());
+    }
+    public void startGreen()
+    {
+        Debug.Log("startGreen");
+        StopAllCoroutines();
+        StartCoroutine(ChangeColorToGreen());
+    }
+    public void startYellow()
+    {
+        Debug.Log("startYellow");
+        StopAllCoroutines();
+        StartCoroutine(ChangeColorToYellow());
+    }
+    public void startMagenta()
+    {
+        Debug.Log("startMagenta");
+        StopAllCoroutines();
+        StartCoroutine(ChangeColorToMagenta());
+    }
+
+//    public static void CopyToClipboard(this string s)
+//    {
+//        TextEditor te = new TextEditor();
+//        te.text = s;
+//        te.SelectAll();
+//        te.Copy();
+//    }
+
+    
+    public void copyMe()
+    {
+
+        if (colorFlashing == "Red")
+        {
+            GUIUtility.systemCopyBuffer = "My FlashR = Red";
+        }
+
+        if (colorFlashing == "Blue")
+        {
+            GUIUtility.systemCopyBuffer = "My FlashR = Blue";
+        }
+
+        if (colorFlashing == "Green")
+        {
+            GUIUtility.systemCopyBuffer = "My FlashR = Green";
+        }
+
+        if (colorFlashing == "Yellow")
+        {
+            GUIUtility.systemCopyBuffer = "My FlashR = Yellow";
+        }
+
+        if (colorFlashing == "Magenta")
+        {
+            GUIUtility.systemCopyBuffer = "My FlashR = Purple";
+        }
+
+    }
+    
 
     public void stopTimer()
     {
@@ -31,11 +101,26 @@ public class Flash3 : MonoBehaviour
         {
             image.color = Color.red;
         }
+
         if (colorFlashing == "Blue")
         {
             image.color = Color.blue;
         }
 
+        if (colorFlashing == "Green")
+        {
+            image.color = Color.green;
+        }
+
+        if (colorFlashing == "Yellow")
+        {
+            image.color = Color.yellow;
+        }
+
+        if (colorFlashing == "Magenta")
+        {
+            image.color = Color.magenta;
+        }
     }
 
     public void fastTimer()
@@ -46,6 +131,25 @@ public class Flash3 : MonoBehaviour
             StartCoroutine(ChangeColorToRed());
         }
 
+        if (colorFlashing == "Blue")
+        {
+            StartCoroutine(ChangeColorToBlue());
+        }
+
+        if (colorFlashing == "Green")
+        {
+            StartCoroutine(ChangeColorToGreen());
+        }
+
+        if (colorFlashing == "Yellow")
+        {
+            StartCoroutine(ChangeColorToYellow());
+        }
+
+        if (colorFlashing == "Magenta")
+        {
+            StartCoroutine(ChangeColorToMagenta());
+        }
         timer = 0.5f;
     }
 
@@ -57,6 +161,25 @@ public class Flash3 : MonoBehaviour
             StartCoroutine(ChangeColorToRed());
         }
 
+        if (colorFlashing == "Blue")
+        {
+            StartCoroutine(ChangeColorToBlue());
+        }
+
+        if (colorFlashing == "Green")
+        {
+            StartCoroutine(ChangeColorToGreen());
+        }
+
+        if (colorFlashing == "Yellow")
+        {
+            StartCoroutine(ChangeColorToYellow());
+        }
+
+        if (colorFlashing == "Magenta")
+        {
+            StartCoroutine(ChangeColorToMagenta());
+        }
         timer = 0.25f;
     }
 
@@ -68,6 +191,25 @@ public class Flash3 : MonoBehaviour
             StartCoroutine(ChangeColorToRed());
         }
 
+        if (colorFlashing == "Blue")
+        {
+            StartCoroutine(ChangeColorToBlue());
+        }
+
+        if (colorFlashing == "Green")
+        {
+            StartCoroutine(ChangeColorToGreen());
+        }
+
+        if (colorFlashing == "Yellow")
+        {
+            StartCoroutine(ChangeColorToYellow());
+        }
+
+        if (colorFlashing == "Magenta")
+        {
+            StartCoroutine(ChangeColorToMagenta());
+        }
         timer = 0.10f;
     }
 
@@ -76,7 +218,12 @@ public class Flash3 : MonoBehaviour
     {
         Debug.Log("ChangeColorToRed");
         colorFlashing = "Red";
+        FlashRText.text = "FlashR";
+        FlashRText.color = Color.black;
         image.color = Color.red;
+
+        GameObject.Find("ButtonTextToCopy").GetComponentInChildren<Text>().text = "My FlashR = Red";
+
         yield return new WaitForSeconds(timer);
         StartCoroutine(ChangeColorToRedBlack());
     }
@@ -85,6 +232,8 @@ public class Flash3 : MonoBehaviour
     {
         Debug.Log("ChangeColorToRedBlack");
         image.color = Color.black;
+        FlashRText.text = "FlashR";
+        FlashRText.color = Color.red;
         yield return new WaitForSeconds(timer);
         StartCoroutine(ChangeColorToRed());
     }
@@ -94,7 +243,12 @@ public class Flash3 : MonoBehaviour
     public IEnumerator ChangeColorToBlue()
     {
         colorFlashing = "Blue";
+        FlashRText.text = "FlashR";
+        FlashRText.color = Color.black;
         image.color = Color.blue;
+
+        GameObject.Find("ButtonTextToCopy").GetComponentInChildren<Text>().text = "My FlashR = Blue";
+
         yield return new WaitForSeconds(timer);
         StartCoroutine(ChangeColorToBlueBlack());
     }
@@ -102,10 +256,90 @@ public class Flash3 : MonoBehaviour
     public IEnumerator ChangeColorToBlueBlack()
     {
         image.color = Color.black;
+        FlashRText.text = "FlashR";
+        FlashRText.color = Color.blue;
         yield return new WaitForSeconds(timer);
         StartCoroutine(ChangeColorToBlue());
     }
     ///////  END BLUE /////////////
+
+    ///////  GREEN /////////////
+    public IEnumerator ChangeColorToGreen()
+    {
+        Debug.Log("ChangeColorToGreen");
+        colorFlashing = "Green";
+        image.color = Color.green;
+        FlashRText.text = "FlashR";
+        FlashRText.color = Color.black;
+
+        GameObject.Find("ButtonTextToCopy").GetComponentInChildren<Text>().text = "My FlashR = Green";
+
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(ChangeColorToGreenBlack());
+    }
+
+    public IEnumerator ChangeColorToGreenBlack()
+    {
+        Debug.Log("ChangeColorToGreenBlack");
+        image.color = Color.black;
+        FlashRText.text = "FlashR";
+        FlashRText.color = Color.green;
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(ChangeColorToGreen());
+    }
+    ///////  END GREEN /////////////
+
+    ///////  YELLOW /////////////
+    public IEnumerator ChangeColorToYellow()
+    {
+        Debug.Log("ChangeColorToYellow");
+        colorFlashing = "Yellow";
+        image.color = Color.yellow;
+        FlashRText.text = "FlashR";
+        FlashRText.color = Color.black;
+
+        GameObject.Find("ButtonTextToCopy").GetComponentInChildren<Text>().text = "My FlashR = Yellow";
+
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(ChangeColorToYellowBlack());
+    }
+
+    public IEnumerator ChangeColorToYellowBlack()
+    {
+        Debug.Log("ChangeColorToRedBlack");
+        image.color = Color.black;
+        FlashRText.text = "FlashR";
+        FlashRText.color = Color.yellow;
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(ChangeColorToYellow());
+    }
+    ///////  END YELLOW /////////////
+
+    ///////  MAGENTA /////////////
+    public IEnumerator ChangeColorToMagenta()
+    {
+        Debug.Log("ChangeColorToMagenta");
+        colorFlashing = "Magenta";
+        image.color = Color.magenta;
+        FlashRText.text = "FlashR";
+        FlashRText.color = Color.black;
+
+        GameObject.Find("ButtonTextToCopy").GetComponentInChildren<Text>().text = "My FlashR = Purple";
+
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(ChangeColorToMagentaBlack());
+    }
+
+    public IEnumerator ChangeColorToMagentaBlack()
+    {
+        Debug.Log("ChangeColorToMagentaBlack");
+        image.color = Color.black;
+        FlashRText.text = "FlashR";
+        FlashRText.color = Color.magenta;
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(ChangeColorToMagenta());
+    }
+    ///////  END MAGENTA /////////////
 
     // Update is called once per frame
     void Update()
@@ -113,9 +347,5 @@ public class Flash3 : MonoBehaviour
 
     }
 
-
-
-
-
-    
+             
 }
